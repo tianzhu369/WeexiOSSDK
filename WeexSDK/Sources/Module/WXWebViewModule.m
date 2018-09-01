7b6570d07323d14be503185f3ba76cf8d0acf1f6
张天柱 <tianzhu.zhang@yiducloud.cn>
2018-09-02 00:18:22 +0800

    postmessage

--- WeexSDK/Sources/Module/WXWebViewModule.m
+++ WeexSDK/Sources/Module/WXWebViewModule.m
@@ -27,6 +27,7 @@
 @synthesize weexInstance;
 
 WX_EXPORT_METHOD(@selector(notifyWebview:data:))
+WX_EXPORT_METHOD(@selector(postMessage:data:))
 WX_EXPORT_METHOD(@selector(reload:))
 WX_EXPORT_METHOD(@selector(goBack:))
 WX_EXPORT_METHOD(@selector(goForward:))
@@ -57,6 +58,12 @@ WX_EXPORT_METHOD(@selector(goForward:))
 - (void)notifyWebview:(NSString *)elemRef data:(NSDictionary *)data {
     [self performBlockWithWebView:elemRef block:^void (WXWebComponent *webview) {
         [webview notifyWebview:data];
+    }];
+}
+
+- (void)postMessage:(NSString *)elemRef data:(NSDictionary *)data {
+    [self performBlockWithWebView:elemRef block:^void (WXWebComponent *webview) {
+        [webview postMessage:data];
     }];
 }
 
